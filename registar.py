2577ae30bfc7332fb6dc9a20a26c6d7923c5738a
ijelonek <ijelonek@live.co.uk>
2016-10-24 19:54:40 +0100

    css and html linked

--- registar.py
+++ registar.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
-app.route('/main')
+@app.route('/', methods = ['GET'])
+def render_temp():
+    return render_template("main.html")
+
+
+
+# if __name__ == 'main'
+app.run(debug=True)
