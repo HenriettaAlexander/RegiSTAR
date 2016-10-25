b9ce01f4f46734aabcef7e21656a1a9519fe24be
ijelonek <ijelonek@live.co.uk>
2016-10-25 10:34:43 +0100

    fixed css import, Jquery added
    I’ve managed to get the name, last name and company displayed and
    separate it into divs. Also managed to get a very simple ticking off
    mechanism in place with jquery.

--- registar.py
+++ registar.py
@@ -4,42 +4,36 @@ from flask import request
 import csv
 
 with open('names.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-
-    # trying to create a dictionary instead of two arrays
-    # attendees = {}
+    readCSV = csv.DictReader(csvfile, delimiter=',')
 
     first_names = []
     last_names = []
+    companies = []
 
     for row in readCSV:
-        first_name = row[1]
-        last_name = row[2]
-
-        first_names.append(first_name)
-        last_names.append(last_name)
-
-        # replacing arrays with dictionary
-        # attendees.append(first_names)
-
-
-    # for i in range(len(first_names)-1):
-    #     print first_names[i], last_names[i]
+        first_names.append(row['first_name'])
+        last_names.append(row['last_name'])
+        companies.append(row['company'])
 
 app = Flask(__name__)
 @app.route('/', methods = ['GET'])
 def render_main():
-    return render_template("main.html", f_names=first_names, l_names=last_names)
+    return render_template("main.html", f_names=first_names, l_names=last_names, companies = companies)
 
 @app.route('/signup', methods = ['POST'])
 def render_signup():
     return render_template("signup_screen.html")
 
+
+# get this function to write to existent excel file so that new person is included on the list
 @app.route('/signup', methods = ['POST'])
 def new_signup():
     form_data = request.form
-    form_info = open("new_signups.csv","a")
-    form_info.write(form_data['name'] + "," + form_data['email'])
+    fields = ["first", "second", "third"]
+    with open('new_signups.csv') as csvfile:
+        form_info = open("new_signups.csv","a")
+        form_info.write(form_data['name'] + "," + form_data['email'])
+        form_info = close("new_signups.csv")
 
 if __name__ == '__main__':
     app.run(debug=True)
